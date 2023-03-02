0011f8d66e331b660b771dd9cdb6c27ac09f4651
Kaseioo <81521485+Kaseioo@users.noreply.github.com>
2023-03-02 11:17:56 -0300

    Enumerate target node order

--- arrows.py
+++ arrows.py
@@ -104,18 +104,29 @@ size = len(available_nodes)
 count = 0
 h = 0;
 target_index = -1
+coun = 0
+ban_x = []
+ban_y = []
 while(size > 5):    
     index = randint(0, size)
     target = available_nodes[index]
     target_index = available_nodes.index(target)
+    if target['x'] in ban_x and target['y'] in ban_y:
+        continue
     if target['x'] == xxx[h] and target['y'] == yyy[h]: continue
+    ban_x.append(xxx[h])
+    ban_y.append(yyy[h])
+    coun += 1
     size -= 1
-    print(target['x'])
-    plt.arrow(xxx[h], yyy[h], target['x'] - xxx[h], target['y'] - yyy[h], color='red', head_width=0.25, head_length=0.5, head_starts_at_zero=False)
+
+    plt.plot([xxx[h], target['x']], [yyy[h], target['y']], color='red')
+    #plt.arrow(xxx[h], yyy[h], target['x'] - xxx[h], target['y'] - yyy[h], color='red', head_width=0.25, head_length=0.5, head_starts_at_zero=False)
     plt.scatter(xxx[h], yyy[h], color='green')
     plt.scatter(target['x'], target['y'], color='blue')
+
+    plt.annotate(coun, xy = (target['x'] + 0.15, target['y']))
     count += 1
     h = target_index
-    if count >= 10: break # change to increase arrows
+    if count >= 7: break # change to increase arrows
         
 plt.show()
