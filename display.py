ef74fc4f30ee03c63c4003fb7cf9bad4749a94da
Kaseioo <81521485+Kaseioo@users.noreply.github.com>
2023-03-04 22:15:10 -0300

    Rendering (?)
    This has 10x less FPS than clicker.py
    
    simply amazing

--- display.py
+++ display.py
@@ -3,36 +3,68 @@ from pygame.locals import *
 
 class Display:
     surfaces = {}
+    components = {}
+    average_fps = [0]
+    current_tick = 0
     
     def __init__(self, width, height):
         self.width  = width
         self.height = height
-        display = pygame.display.set_mode((width, height))
-        self.surfaces['display']  = display
+        self.display = pygame.display.set_mode((width, height))
+        self.surfaces['display']  = self.display
         
-        self.font   = pygame.font.SysFont('Arial', 18)
         self.clock  = pygame.time.Clock()
 
         
-    def render(self, surface, x, y):
-        self.surfaces['display'].blit(self.surfaces[surface], (x, y))
-        pygame.display.update()
+    def render(self, surface, component):
+        x = component.get_x()
+        y = component.get_x()
+        self.surfaces[surface].blit(component.component, (x, y))
+        
+    def render_surface(self, surface):
+        print('rendering', surface)
+        self.display.blit(self.surfaces[surface], (0, 0))
         
     def fill(self, surface, color, x = 0, y = 0, width = None, height = None):
         self.surfaces[surface].fill(color, (x, y, width, height))
         
-    def update(self):
-        for surface in self.surfaces:
-            if surface == 'display':
-                continue
-            self.render(surface, 0, 0)
-        #pygame.display.update()
+    def update(self, surface = None):
+        if self.current_tick >= 0:
+            if surface is not None:
+                self.render_surface(surface)
+                
+            for component in self.components:
+                surface = self.components[component]['surface']
+                element = self.components[component]['component']
+                self.render(surface, element)
+        pygame.display.update()
+        
         
     def tick(self, fps):
+        self.average_fps.append(self.clock.get_fps())
+        if(len(self.average_fps) > 60 * 10):
+            self.average_fps.pop(0)
+            
+        self.current_tick += 1
+        if(self.current_tick > 60):
+            self.current_tick = 0
+            
         self.clock.tick(fps)
+        
+    def get_fps(self):
+        average_fps = sum(self.average_fps) / len(self.average_fps)
+        return average_fps
         
     def add_surface(self, name, surface):
         self.surfaces[name] = surface
+    
+    def add_component(self, name, component, surface = 'components'):
+        self.render(surface, component)
+        self.components[name] = {'component': component, 'surface': surface}
+        
+    def update_component(self, name, component):
+        self.components[name]['component'] = component
+        self.render(self.components[name]['surface'], component)
         
     
     
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from display import Display
 from events import EventHandler
+from components import *
 import pygame
 from pygame.locals import *
 
@@ -10,19 +11,25 @@ GREEN       = (0, 255, 0)
 BLUE        = (0, 0, 255)
 VIOLET      = (255, 0, 255)
 
-HEIGHT = 600
-WIDTH = 800
-BOUNDS = (WIDTH, HEIGHT)
+HEIGHT  = 600
+WIDTH   = 800
+BOUNDS  = (WIDTH, HEIGHT)
+START   =  (0, 0)
 
 pygame.init()
 display = Display(WIDTH, HEIGHT)
+screen  = display.surfaces['display']
+event_handler = EventHandler(display)
+
 display.add_surface('points',   pygame.Surface(BOUNDS, pygame.SRCALPHA))
 display.add_surface('lines',    pygame.Surface(BOUNDS, pygame.SRCALPHA))
+display.add_surface('components', pygame.Surface(BOUNDS, pygame.SRCALPHA))
 
+display.fill('components', GRAY, 0, 0, WIDTH, HEIGHT/5)
 
-event_handler = EventHandler(display)
-display.fill('display', GRAY, 0, 0, 800, 600)
-display.render('display', 0, 0)
+fps = TextComponent(0, START)
+display.add_component('fps', fps)
+display.render_surface('components')
 display.update()
 
 
@@ -31,7 +38,12 @@ ADD_NODE = 1
 while True:
     event_handler.update()
     if(event_handler.proccess_events()):
-        display.update()
-
+        print(event_handler.get_events())
+    print(display.get_fps())
+    # display.render_surfaces('clean_components')
+    fps.set_text(str(round(display.get_fps(), 2)) + ' fps')
     
-    display.tick(60)
+    display.fill('components', GRAY, fps.get_x(), fps.get_x(), fps.get_width() + 5, fps.get_height())
+    display.update_component('fps', fps)
+    display.update('components')
+    display.tick(100000)
