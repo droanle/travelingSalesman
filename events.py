bf3c0966e10b6594fc0edcbca93f3b15a7d1fb90
Replit user <>
2023-05-11 07:49:11 -0300

    Subida de encosta V1

--- events.py
+++ events.py
@@ -1,22 +1,25 @@
 import pygame
 
+
 class EventHandler:
+    subscribers = {}
+
     def __init__(self, origin):
         self.event = None
         self.events = []
         self.origin = origin
-        
+
     def get_events(self):
         event = self.event
         self.event = None
         return event
-    
+
     def proccess_events(self):
-        if self.event is not None: 
+        if self.event is not None:
             self.event = None
             return True
         return False
-    
+
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,18 +27,17 @@ class EventHandler:
                 pygame.display.quit()
                 pygame.quit()
                 exit()
-                
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.event = 1
                 self.mouse_button_down(event)
-                
+
             if event.type == pygame.KEYDOWN:
                 self.key_down(event)
-            
-    
+
     def mouse_button_down(self, event):
         if event.button == 1:
-            print(event.pos)
+            self.alert_listeners(1, event.pos)
         elif event.button == 2:
             pass
         elif event.button == 3:
@@ -43,10 +45,25 @@ class EventHandler:
         else:
             print('no event')
         pass
-                
+
     def key_down(self, event):
         if event.key == pygame.K_ESCAPE:
             pygame.display.quit()
             pygame.quit()
             exit()
         pass
+
+    def alert_listeners(self, button, data):
+        for listener in self.subscribers[button]:
+            if listener["handler"].validate(listener["condition"], data):
+                listener["handler"].ignite()
+
+    def add_subscribe(self, button, handler, condition):
+        subscriber = {}
+        subscriber["handler"] = handler
+        subscriber["condition"] = condition
+
+        if not self.subscribers.get(button):
+            self.subscribers[button] = []
+
+        self.subscribers[button].append(subscriber)
--- main.py
+++ main.py
@@ -1,51 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 07:53:03 2023
+
+@author: aluno
+"""
+
 from display import Display
 from events import EventHandler
+from hillClimb import HillClimb
 from components import *
 import pygame
 from pygame.locals import *
 
-BLACK       = (0, 0, 0)
-GRAY        = (100, 100, 100)
-RED         = (255, 0, 0)
-GREEN       = (0, 255, 0)
-BLUE        = (0, 0, 255)
-VIOLET      = (255, 0, 255)
+BLACK = (0, 0, 0)
+GRAY = (40, 40, 40)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+VIOLET = (255, 0, 255)
+WHITE = (255, 255, 255)
 
-HEIGHT  = 600
-WIDTH   = 800
-BOUNDS  = (WIDTH, HEIGHT)
-START   =  (0, 0)
+HEIGHT = 400
+WIDTH = 800
+BORDER_WIDTH = 4
+BOUNDS = (WIDTH, HEIGHT)
+START = (0, 0)
 
 pygame.init()
 display = Display(WIDTH, HEIGHT)
-screen  = display.surfaces['display']
+screen = display.surfaces['display']
+font_25 = pygame.font.SysFont(None, 25)
+font_35 = pygame.font.SysFont(None, 35)
+
 event_handler = EventHandler(display)
 
-display.add_surface('points',   pygame.Surface(BOUNDS, pygame.SRCALPHA))
-display.add_surface('lines',    pygame.Surface(BOUNDS, pygame.SRCALPHA))
-display.add_surface('components', pygame.Surface(BOUNDS, pygame.SRCALPHA))
+# [10, 50][185, 50]
+# [185, 75][10, 75]
 
-display.fill('components', GRAY, 0, 0, WIDTH, HEIGHT/5)
+# (  2,   4),         (  7,   4),
+# (  2,   2),         (  7,   2)
+# ( 10,  50),         (185,  50)
+# ( 10,  75)          (185,  75)
+# Blocks
+display.add_surface('methods', pygame.Surface(BOUNDS, pygame.SRCALPHA), {
+    "local": [0, 0],
+    "width": "35%",
+    "height": "100%",
+    "fill-color": "GRAY"
+})
+display.add_surface(
+    'map', pygame.Surface(BOUNDS, pygame.SRCALPHA), {
+        "x-local": ["right", "methods"],
+        "y-local": 0,
+        "width": "65%",
+        "height": "75%",
+        "border": str(BORDER_WIDTH) + "size",
+        "fill-color": "GRAY"
+    })
+display.add_surface(
+    'init_solution', pygame.Surface(BOUNDS, pygame.SRCALPHA), {
+        "x-local": ["right", "methods"],
+        "y-local": ["bottom", "map"],
+        "width": "65%",
+        "height": "25%",
+        "border": str(BORDER_WIDTH) + "size",
+        "fill-color": "GRAY"
+    })
 
-fps = TextComponent(0, START)
-display.add_component('fps', fps)
-display.render_surface('components')
+mapStruture = display.strutures.get('map')
+
+event_handler.add_subscribe(
+    1,
+    HillClimb("Subida de encosta", display, mapStruture['local'],
+              [mapStruture['height'], mapStruture['width']], BORDER_WIDTH,
+              RED), {
+                  "init": [10, 50],
+                  "form": [175, 25]
+              })
+
+# Texts
+display.add_surface(
+    'txt_sud-de-encost',
+    pygame.font.SysFont(None, 25).render(' ▣ Subida de encosta', True, GREEN))
+display.add_surface(
+    'txt_metodo',
+    pygame.font.SysFont(None, 35).render('Métodos', True, GREEN))
+display.add_surface(
+    'txt_sud-de-encost-alter',
+    pygame.font.SysFont(None, 25).render('Subida de encosta alterado', True,
+                                         GREEN))
+display.add_surface(
+    'txt_tentativa',
+    pygame.font.SysFont(None, 23).render(' -- Tentativa', True, GREEN))
+
+# Fill Blocks
+display.fill('methods')
+display.fill('map')
+display.fill('init_solution')
+
+# Render Surface
+display.render_surface('methods')
+display.render_surface('map')
+display.render_surface('init_solution')
+
+display.render_surface('txt_metodo', (BORDER_WIDTH * 3, BORDER_WIDTH * 4))
+display.render_surface(
+    'txt_sud-de-encost',
+    (BORDER_WIDTH * 3, (BORDER_WIDTH * 4) + 35),
+)
+
+# display.render_surface('txt_sud-de-encost-alter',
+#                        (BORDER_WIDTH * 3, ((BORDER_WIDTH * 4) + 28 * 2)))
+
+# display.render_surface('txt_tentativa', (((BORDER_WIDTH * 3) + 5),
+#                                          ((BORDER_WIDTH * 4) + 25 * 3)))
+
+# Update Display
 display.update()
 
-
-ADD_NODE = 1
-
-counter = 0
 while True:
-    counter += 1
     event_handler.update()
-    if(event_handler.proccess_events()):
-        print(event_handler.get_events())
-    
 
-    # display.render_surfaces('clean_components')
-    fps.set_text(str(round(display.get_fps(), 2)) + ' fps')
-    
-    display.fill('components', GRAY, fps.get_x(), fps.get_x(), fps.get_width() + 5, fps.get_height())
-    display.update_component('fps', fps)
     display.tick(1000)
